62f0e8120a764f2500f46b63c8a7e076a77c1432
John <John@qq.com>
2024-06-22 12:48:14 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,33 @@
-window.ITInjectSetupWebViewJavascriptBridge(function(bridge) {
+
+
+function test() {
+  window.ITInjectSetupWebViewJavascriptBridge(function(bridge) {
+
+    console.log(222222);
 	
-  bridge.registerHandler('JS Echo', function(data, responseCallback) {
-		console.log("JS Echo called with:", data);
-		responseCallback(data);
-	});
-
-	bridge.callHandler('ObjC Echo', {'key':'value'}, function responseCallback(responseData) {
-		console.log("JS received response:", responseData);
-	});
-
-  bridge.callHandler('getInfo', { 
-    'cfg': ['version', 'atIntervals', 'atDurations', 'hehehe'], 
-    'ua': ['version', 'timestamp', 'userAgent', 'lilili'], 
-    'base': ['deviceID', 'appBundleID', 'appVersion', 'batteryLevel', 'carrier', 'lalala'] 
-  }, function(responseData) {
-    setTimeout(() => {
-      console.log(responseData);
-    }, 5000);
-  });
+    bridge.registerHandler('JS Echo', function(data, responseCallback) {
+      console.log("JS Echo called with:", data);
+      responseCallback(data);
+    });
   
-});
+    bridge.callHandler('ObjC Echo', {'key':'value'}, function responseCallback(responseData) {
+      console.log("JS received response:", responseData);
+    });
+  
+    bridge.callHandler('getInfo', { 
+      'cfg': ['version', 'atIntervals', 'atDurations', 'hehehe'], 
+      'ua': ['version', 'timestamp', 'userAgent', 'lilili'], 
+      'base': ['deviceID', 'appBundleID', 'appVersion', 'batteryLevel', 'carrier', 'lalala'] 
+    }, function(responseData) {
+      setTimeout(() => {
+        console.log(responseData);
+      }, 5000);
+    });
+    
+  });
+}
+
+setTimeout(() => {
+  console.log(1111111);
+  test();
+}, 5000);
